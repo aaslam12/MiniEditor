2eb36620409d535951acb936029d1f685583a555
Altamash Aslam <aaslam12@users.noreply.github.com>
2025-11-22 22:43:13 -0700

    Initial commit. Create skeleton of files and sample test file

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a simple text editor created to be used in terminals.
---
+++ tests/test_buffer.cpp
@@ -0,0 +1,10 @@
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("basic placeholder")
+{
+    REQUIRE((1 + 1) == 2);
+    REQUIRE((1 + 1) == 2);
+    REQUIRE((1 + 1) == 2);
+    REQUIRE((1 + 1) == 2);
+    REQUIRE((1 + 1) == 2);
+}
